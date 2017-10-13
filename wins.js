3398b7580778b2ee09d535d3d44d6899ca4f3fd4
genglefr <nufann@gmail.com>
2017-10-13 11:23:54 +0200

    Remove unused views and style menu

--- wins.js
+++ wins.js
@@ -1,9 +1,11 @@
 var electronInstaller = require('electron-winstaller');
 resultPromise = electronInstaller.createWindowsInstaller({
     appDirectory: './release-builds/MyGallery-win32-ia32',
+    loadingGif : './build/spikeseed_white_loading.gif',
     outputDirectory: './release-builds/MyGallery-win32-installer',
     authors: 'Genglefr',
-    exe: 'MyGallery.exe'
+    exe: 'MyGallery.exe',
+    setupIcon : './build/spikeseed_vXb_icon.ico'
 });
 
-resultPromise.then(() => console.log("It worked!"), (e) => console.log(`No dice: ${e.message}`));
+resultPromise.then(() => console.log("Done."), (e) => console.log(`Error: ${e.message}`));
