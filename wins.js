34dabe0cfb1831f8a20274d97ad53a9e7c4eaad9
genglefr <nufann@gmail.com>
2017-11-28 12:05:06 +0100

    fix bower dependencies and fork image gallery

--- wins.js
+++ wins.js
@@ -3,7 +3,7 @@ resultPromise = electronInstaller.createWindowsInstaller({
     appDirectory: './release-builds/MyGallery-win32-ia32',
     loadingGif : './build/spikeseed_white_loading.gif',
     outputDirectory: './release-builds/MyGallery-win32-installer',
-    authors: 'Genglefr',
+    authors: 'genglefr',
     exe: 'MyGallery.exe',
     setupIcon : './build/spikeseed_vXb_icon.ico'
 });
