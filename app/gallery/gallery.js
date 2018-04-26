4930e21acee3ef3f7fc4520466364a83841fe1a9
genglefr <Anderlecht8>
2018-04-26 11:25:24 +0200

    Create WS adapter

--- app/gallery/gallery.js
+++ app/gallery/gallery.js
@@ -12,7 +12,8 @@ angular.module('myApp.gallery', ['ngRoute','toastr','pouchdb','thatisuday.ng-ima
     var remoteDb = new PouchDB(remoteUrl);
     var opts = {live: true, back_off_function: back_off, checkpoint: false};
 
-    initWebsocket(localDb, remoteDb);
+    var adapter = new PouchWsAdapter(localDb, remoteDb);
+    adapter.sync();
     //localDb.replicate.from(remoteUrl, opts, syncError);
     localDb.replicate.to(remoteUrl, opts, syncError);
 
@@ -126,73 +127,4 @@ angular.module('myApp.gallery', ['ngRoute','toastr','pouchdb','thatisuday.ng-ima
             });
         }
     }
-
-    function checkpoint(change){
-        var checkpointCount = 2;
-        if ((change.seq % checkpointCount) == 0 && !window.checkpointing) {
-            window.checkpointing = true;
-            localDb.get("_local/checkpoint", {revs: true}).then(function (doc) {
-                doc.seq = change.seq;
-                doc.docId = change.id;
-                doc.rev = change.changes[0].rev;
-                localDb.put(doc);
-            }).catch(function () {
-                localDb.put({
-                    _id: "_local/checkpoint",
-                    seq: change.seq,
-                    docId: change.id,
-                    rev: change.changes[0].rev
-                });
-            }).finally(function(){
-                window.checkpointing = false;
-            });
-        }
-    }
-
-    function initWebsocket(localDb, remoteDb, forceCreate) {
-        if(!window.s || forceCreate)
-            window.s = new WebSocket("ws://localhost:4984/images/_changes?feed=websocket");
-        s.onmessage = function (event) {
-            if (event.data) {
-                var changes = JSON.parse(event.data);
-                changes.forEach(function (change) {
-                    console.log(change);
-                    remoteDb.get(change.id,{rev: change.changes.length > 0 ? change.changes[0].rev : '', revs: true}).then(function (doc) {
-                        localDb.bulkDocs([doc], {new_edits: false}).then(function () {
-                            checkpoint(change)
-                        });
-                    }).catch(function(err){
-                        console.log("Error when replicating change '" + change.id + "'. Could be that doc was removed meanwhile. Error:" + JSON.stringify(err));
-                    });
-                });
-            }
-        }
-        var getCheckpoint = new Promise(function(resolve, reject){
-            localDb.get("_local/checkpoint").then(function (doc) {
-                return resolve(doc.seq);
-            }).catch(function () {
-                localDb.info().then(function (result) {
-                    return resolve(result.update_seq);
-                }).catch(function(){
-                    return resolve(0);
-                });
-            });
-        })
-        s.onopen = function (event) {
-            getCheckpoint.then(function(seq) {
-                var text = JSON.stringify({
-                    "since": seq,
-                    "style": 'all_docs'
-                });
-                console.log("Starting sync with following options: " + text);
-                var encoded = new TextEncoder("ascii").encode(text);
-                s.send(encoded);
-            });
-        };
-        s.onclose = function () {
-            //reinit the socket if the page is still open
-            console.log("Attempt to close the websocket");
-            initWebsocket(localDb, remoteDb, true);
-        };
-    };
 }]);
---
+++ app/gallery/pouch-ws-adapter.js
@@ -0,0 +1,101 @@
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+        typeof define === 'function' && define.amd ? define(factory) :
+            (global.PouchWsAdapter = factory());
+}(this, (function () {'use strict';
+    function PouchWsAdapter(localDb, remoteDb, options) {
+        var self = this;
+        if (!localDb || !remoteDb) {
+            throw new Error("Please provide a local and a remote db.");
+        }
+        this.localDb = localDb;
+        this.remoteDb = remoteDb;
+        var opts = options || {};
+        this.forceCreate = opts.forceCreate || true;
+        this.keepAlive = opts.keepAlive || false;
+        this.queryParams = opts.queryParams || '';
+        this.checkpointCount = opts.checkpointCount || 2;
+        this._initWebsocket();
+        this.socket.onmessage = function (event) {
+            if (event.data) {
+                var changes = JSON.parse(event.data);
+                changes.forEach(function (change) {
+                    //console.log(change);
+                    self.remoteDb.get(change.id,{rev: change.changes.length > 0 ? change.changes[0].rev : '', revs: true}).then(function (doc) {
+                        //console.log(doc);
+                        self.localDb.bulkDocs([doc], {new_edits: false}).then(function () {
+                            self._checkpoint(change)
+                        });
+                    }).catch(function(err){
+                        console.log("Error when replicating change '" + change.id + "'. Could be that doc was removed meanwhile. Error:" + JSON.stringify(err));
+                    });
+                });
+            }
+        }
+        this.getCheckpoint = new Promise(function(resolve, reject){
+            self.localDb.get("_local/checkpoint").then(function (doc) {
+                return resolve(doc.seq);
+            }).catch(function () {
+                self.localDb.info().then(function (result) {
+                    return resolve(result.update_seq);
+                }).catch(function(){
+                    return resolve(0);
+                });
+            });
+        })
+        if (this.keepAlive) {
+            this.socket.onclose = function () {
+                //reinit the socket if the page is still open
+                //console.log("Attempt to close the websocket");
+                self._initWebsocket();
+            };
+        }
+    };
+
+    PouchWsAdapter.prototype._initWebsocket = function () {
+        if (!this.socket || this.forceCreate) {
+            var remoteUrl = this.remoteDb.name.replace("http", "ws");
+            this.socket = new WebSocket(remoteUrl + "/_changes?feed=websocket");
+        }
+    }
+
+    PouchWsAdapter.prototype.sync = function () {
+        var self = this;
+        this.socket.onopen = function (event) {
+            self.getCheckpoint.then(function(seq) {
+                var text = JSON.stringify({
+                    "since": seq,
+                    "style": 'all_docs',
+                    "query_params" : self.queryParams
+                });
+                //console.log("Starting sync with following options: " + text);
+                var encoded = new TextEncoder("ascii").encode(text);
+                self.socket.send(encoded);
+            });
+        };
+    }
+
+    PouchWsAdapter.prototype._checkpoint = function (change){
+        var self = this;
+        if ((change.seq % this.checkpointCount) == 0 && !self.checkpointing) {
+            self.checkpointing = true;
+            self.localDb.get("_local/checkpoint", {revs: true}).then(function (doc) {
+                doc.seq = change.seq;
+                doc.docId = change.id;
+                doc.rev = change.changes[0].rev;
+                self.localDb.put(doc);
+            }).catch(function () {
+                self.localDb.put({
+                    _id: "_local/checkpoint",
+                    seq: change.seq,
+                    docId: change.id,
+                    rev: change.changes[0].rev
+                });
+            }).finally(function(){
+                self.checkpointing = false;
+            });
+        }
+    }
+
+    return PouchWsAdapter;
+})));
